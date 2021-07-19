30bec7c8028dd53d4a1dc451d0534963413c35d8
muizogundiran <muizakanji@gmail.com>
2021-07-19 22:48:28 +0100

    Re build

--- web/src/components/Question.jsx
+++ web/src/components/Question.jsx
@@ -1,7 +1,7 @@
 import Code from './Code' ;
 const Question = ({ question }) => {
     return (
-        <div className="row-span-5 h-full bg-githubTheme-blue300 bg-opacity-5 col-span-4 md:col-span-2  rounded-md p-4">
+        <div className="row-span-5 h-full bg-githubTheme-blue300 bg-opacity-5 col-span-4 md:col-span-2 rounded-md p-4">
             <h4 className="font-bold text-2xl text-gray-50 m-4">3. What's the Output</h4>
             <Code>
                 {`
@@ -13,37 +13,7 @@ const name = 'Lydia';
 
 name.giveLydiaPizza();
 
-Repeating 😁
 
-String.prototype.giveLydiaPizza = () => {
-    return 'Just give Lydia pizza already!';
-};
-
-const name = 'Lydia';
-
-name.giveLydiaPizza();
-
-Repeating 😁
-
-String.prototype.giveLydiaPizza = () => {
-    return 'Just give Lydia pizza already!';
-};
-
-const name = 'Lydia';
-
-name.giveLydiaPizza();
-
-Repeating 😁
-
-String.prototype.giveLydiaPizza = () => {
-    return 'Just give Lydia pizza already!';
-};
-
-const name = 'Lydia';
-
-name.giveLydiaPizza();
-
-Repeating 😁
                 `}
             </Code>
         </div>
